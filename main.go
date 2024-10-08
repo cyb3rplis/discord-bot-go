9b1bdb9a01b99253a3c11bd722292de3a63e441c
Drilon Kamberaj <drilonkamberaj@hotmail.com>
2024-10-09 01:54:26 +0200

    feat: connect to voice channel and play sound

--- main.go
+++ main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/signal"
 	"path/filepath"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
+	"github.com/jonas747/dca"
 )
 
 //GOTO: https://discord.com/developers/applications/
-var Token = "<your-token>"
+var Token = "<your-token-here>"
 
 func main() {
 	//new session
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("error creating discord session,", err)
+		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
@@ -54,7 +53,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 	if len(m.Content) == 0 {
-		fmt.Println("empty message > no content")
+		fmt.Println("Empty message content.")
 		return
 	}
 
@@ -68,7 +67,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			s.ChannelMessageSend(m.ChannelID, "No sounds found in the directory.")
 			return
 		}
-		soundList := "Sounds List:\n"
+		soundList := "sounds List:\n"
 		for _, sound := range sounds {
 			soundList += "- " + sound[:len(sound)-4] + "\n" // Remove .mp3 extension
 		}
@@ -88,16 +87,40 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 		soundFile := fmt.Sprintf("./sounds/%s.mp3", args[1])
+		// find the guild and voice state to identify which channel the user is in
+		guildID := m.GuildID
+		userID := m.Author.ID
+		voiceState := getVoiceState(s, guildID, userID)
+
+		if voiceState == nil {
+			s.ChannelMessageSend(m.ChannelID, "You need to be in a voice channel to play sounds.")
+			return
+		}
 
 		//play sound
-		err := playSound(soundFile)
+		err := playSound(s, guildID, voiceState.ChannelID, soundFile)
 		if err != nil {
-			//s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("error playing sound: %v", err))
 			s.ChannelMessageSend(m.ChannelID, "error playing sound. Check if sound exists")
-		} else {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("playing sound: %s", args[1]))
+			return
+		}
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("playing sound: %s", args[1]))
+	}
+}
+
+// function to find the user's voice state in a guild
+func getVoiceState(s *discordgo.Session, guildID, userID string) *discordgo.VoiceState {
+	guild, err := s.State.Guild(guildID)
+	if err != nil {
+		return nil
+	}
+
+	for _, vs := range guild.VoiceStates {
+		if vs.UserID == userID {
+			return vs
 		}
 	}
+
+	return nil
 }
 
 func listSounds() ([]string, error) {
@@ -105,38 +128,79 @@ func listSounds() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	var soundFiles []string
+	baseDir, err := filepath.Abs("./sounds")
+	if err != nil {
+		return nil, err
+	}
 	for _, file := range files {
-		// Only include .mp3 files
-		if filepath.Ext(file.Name()) == ".mp3" {
+		// resolve the absolute path of each file
+		filePath, err := filepath.Abs(filepath.Join("./sounds", file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		// ensure the file is inside the base directory
+		relPath, err := filepath.Rel(baseDir, filePath)
+		if err != nil {
+			return nil, err
+		}
+		// check if the relative path does not escape the base directory
+		if relPath == ".." || relPath[:3] == "../" {
+			return nil, errors.New("potential path traversal detected")
+		}
+		// only append if symlink
+		if filepath.Ext(file.Name()) == ".mp3" && !isSymlink(filePath) {
 			soundFiles = append(soundFiles, file.Name())
 		}
 	}
-  
+
 	return soundFiles, nil
 }
 
 //play sound test
-func playSound(filepath string) error {
-	file, err := os.Open(filepath)
+func playSound(s *discordgo.Session, guildID, channelID, soundFile string) error {
+	// connect to the voice channel
+	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
 	if err != nil {
-		return fmt.Errorf("error opening file: %v", err)
+		return fmt.Errorf("failed to join voice channel: %w", err)
 	}
-	defer file.Close()
 
-	streamer, format, err := mp3.Decode(file)
+	// open the sound file
+	sound, err := os.Open(soundFile)
 	if err != nil {
-		return fmt.Errorf("error decoding mp3: %v", err)
+		return fmt.Errorf("failed to open sound file: %w", err)
 	}
-	defer streamer.Close()
+	defer sound.Close()
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	// create a new DCA encoder
+	encodeSession, err := dca.EncodeFile(soundFile, dca.StdEncodeOptions)
+	if err != nil {
+		return fmt.Errorf("failed to encode sound file: %w", err)
+	}
+	defer encodeSession.Cleanup()
 
-	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-		done <- true
-	})))
+	// play the audio stream
+	done := make(chan error)
+	dca.NewStream(encodeSession, vc, done)
 
-	<-done
+	// wait for the sound to finish playing
+	err = <-done
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error while playing sound: %w", err)
+	}
+
+	// disconnect after playing
+	vc.Disconnect()
+
 	return nil
+}
+
+// helper
+func isSymlink(path string) bool {
+	fileInfo, err := os.Lstat(path)
+	if err != nil {
+		return false
+	}
+	return (fileInfo.Mode() & os.ModeSymlink) != 0
 }
